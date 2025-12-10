d0c76ef48fbf7574dccdec2a585b76741b9663ec
5theldritchabomination <thibault.royer1@ecoles-epsi.net>
2025-12-10 19:22:16 +0100

    interact

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react';
+
 export default function Buttons(turn) {
+  const [played, setPlayed] = useState(0);
   const getClicked = () => {
     if (turn) {
+      setPlayed('1');
     } else {
+      setPlayed('2');
     }
   };
+
   return (
-    <button
-      className="bg-blue-200 p-4 rounded-xl size-15"
-      onClick={getClicked}
-    ></button>
+    <button className="bg-blue-200 p-4 rounded-xl size-15" onClick={getClicked}>
+      {' '}
+      {played}{' '}
+    </button>
   );
 }
